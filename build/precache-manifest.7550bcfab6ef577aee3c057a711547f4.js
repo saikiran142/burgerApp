6720c1ab50846b7a17839ab5bad03941b1371f7e
saikiran142 <ramavathsaikiran123@gmail.com>
2020-08-26 16:15:16 +0530

    build file pushed

---
+++ build/precache-manifest.7550bcfab6ef577aee3c057a711547f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea544bf94d2013b619fa25bd432b8bd",
+    "url": "/burgerApp/index.html"
+  },
+  {
+    "revision": "5f089646e62316b486a2",
+    "url": "/burgerApp/static/css/main.d61a3f01.chunk.css"
+  },
+  {
+    "revision": "f25ac229646b9aa87280",
+    "url": "/burgerApp/static/js/2.7a4aa7e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burgerApp/static/js/2.7a4aa7e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f089646e62316b486a2",
+    "url": "/burgerApp/static/js/main.bf66223b.chunk.js"
+  },
+  {
+    "revision": "5cfeb81812c289e04540",
+    "url": "/burgerApp/static/js/runtime-main.d7d3e5cf.js"
+  }
+]);
